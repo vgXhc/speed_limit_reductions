a9fef1bf817d9db931311831764f13ce28e56efb
Harald Kliems <kliems@gmail.com>
2024-08-12 06:49:04 -0500

    changed color palette

--- speed_limit_chart.R
+++ speed_limit_chart.R
@@ -4,10 +4,12 @@ speed <- read_csv("data/Vision Zero speed limit reduction data .csv")
 
 speed |> 
   mutate(inc_dec = if_else(pct_y_1 > pct_y_2, "decrease", "increase"),
+         inc_dec_pct = pct_y_2 - pct_y_1,
          volume_y_2 = pct_y_2 * n_y_2) |> 
-  ggplot(aes(pct_y_1, pct_y_2, color = inc_dec, size = volume_y_2)) +
+  ggplot(aes(pct_y_1, pct_y_2, color = inc_dec_pct, size = volume_y_2)) +
   geom_point() +
-  scale_color_viridis_d(name = "Increase/decrease") +
+  scale_color_continuous_divergingx(name = "% pt decrease/increase", palette = 'RdBu', mid = 0) +
+  # scale_color_distiller(name = "% pt decrease/increase", type = "div", palette = "BrBG") +
   scale_size(name = "Traffic volume after") +
   geom_abline(slope = 1, intercept = 0, linetype = 3) +
   labs(x = "Before reduction",
